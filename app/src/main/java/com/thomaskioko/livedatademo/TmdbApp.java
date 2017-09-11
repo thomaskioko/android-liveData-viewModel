28ab5005a0af4700d5516a37578a5226b1424c80
kioko <kiokotomas@gmail.com>
2017-09-11 21:38:52 +0300

    make class implement activity injector

---
+++ app/src/main/java/com/thomaskioko/livedatademo/TmdbApp.java
@@ -0,0 +1,41 @@
+package com.thomaskioko.livedatademo;
+
+import android.app.Activity;
+import android.app.Application;
+
+import com.thomaskioko.livedatademo.di.AppInjector;
+
+import javax.inject.Inject;
+
+import dagger.android.AndroidInjector;
+import dagger.android.DispatchingAndroidInjector;
+import dagger.android.HasActivityInjector;
+import timber.log.BuildConfig;
+import timber.log.Timber;
+
+/**
+ * @author Thomas Kioko
+ */
+
+public class TmdbApp extends Application implements HasActivityInjector {
+
+    @Inject
+    DispatchingAndroidInjector<Activity> dispatchingAndroidInjector;
+
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        if (BuildConfig.DEBUG) {
+            Timber.plant(new Timber.DebugTree());
+        }
+
+        AppInjector.init(this);
+    }
+
+    @Override
+    public AndroidInjector<Activity> activityInjector() {
+        return dispatchingAndroidInjector;
+    }
+}
