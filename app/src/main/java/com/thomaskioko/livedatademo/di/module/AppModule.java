6a2a120517fa5b1b9fa68ee04ee7bbf13f1cb853
Thomas Kioko <kiokotomas@gmail.com>
2017-09-12 17:36:02 +0300

    create dagger module classes

---
+++ app/src/main/java/com/thomaskioko/livedatademo/di/module/AppModule.java
@@ -0,0 +1,27 @@
+package com.thomaskioko.livedatademo.di.module;
+
+import com.thomaskioko.livedatademo.repository.api.TmdbService;
+
+import javax.inject.Singleton;
+
+import dagger.Module;
+import dagger.Provides;
+import retrofit2.Retrofit;
+
+/**
+ * @author Thomas Kioko
+ */
+
+@Module(includes = {
+        ViewModelModule.class,
+        NetworkModule.class
+})
+public class AppModule {
+
+    @Provides
+    @Singleton
+    TmdbService provideTmdbService(Retrofit retrofit) {
+        return retrofit.create(TmdbService.class);
+    }
+
+}
