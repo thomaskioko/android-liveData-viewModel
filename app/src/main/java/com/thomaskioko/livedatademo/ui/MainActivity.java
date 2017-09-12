eab873d8abd7fce15712e35862a245f614816062
Thomas Kioko <kiokotomas@gmail.com>
2017-09-12 18:28:54 +0300

    inject ViewModelProvider

--- app/src/main/java/com/thomaskioko/livedatademo/ui/MainActivity.java
+++ app/src/main/java/com/thomaskioko/livedatademo/ui/MainActivity.java
@@ -1,6 +1,8 @@
 package com.thomaskioko.livedatademo.ui;
 
+import android.app.Activity;
 import android.arch.lifecycle.LifecycleActivity;
+import android.arch.lifecycle.ViewModelProvider;
 import android.arch.lifecycle.ViewModelProviders;
 import android.os.Bundle;
 import android.view.View;
@@ -9,21 +11,36 @@ import android.widget.ListView;
 import android.widget.ProgressBar;
 
 import com.thomaskioko.livedatademo.R;
+import com.thomaskioko.livedatademo.di.Injectable;
 import com.thomaskioko.livedatademo.ui.viewmodel.MainActivityViewModel;
 
-public class MainActivity extends LifecycleActivity {
+import javax.inject.Inject;
+
+import dagger.android.AndroidInjection;
+import dagger.android.AndroidInjector;
+import dagger.android.DispatchingAndroidInjector;
+import dagger.android.HasActivityInjector;
+
+public class MainActivity extends LifecycleActivity implements Injectable, HasActivityInjector {
+
+    @Inject
+    DispatchingAndroidInjector<Activity> dispatchingAndroidInjector;
+
+    @Inject
+    ViewModelProvider.Factory viewModelFactory;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        AndroidInjection.inject(this);
+
         ListView listView = findViewById(R.id.list);
         ProgressBar progressBar = findViewById(R.id.progressbar);
 
         progressBar.setVisibility(View.VISIBLE);
-
-        ViewModelProviders.of(this)
+        ViewModelProviders.of(this, viewModelFactory)
                 .get(MainActivityViewModel.class)
                 .getGitUserNames()
                 .observe(this, stringList -> {
@@ -35,5 +52,10 @@ public class MainActivity extends LifecycleActivity {
                     progressBar.setVisibility(View.GONE);
                 });
 
+    }
+
+    @Override
+    public AndroidInjector<Activity> activityInjector() {
+        return dispatchingAndroidInjector;
     }
 }
