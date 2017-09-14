8b57caa76b916247e6a5037aade3c7ed0d756444
Thomas Kioko <kiokotomas@gmail.com>
2017-09-14 17:57:23 +0300

    remove dummy data, load data from endpoints 🤓

--- app/src/main/java/com/thomaskioko/livedatademo/ui/viewmodel/MainActivityViewModel.java
+++ app/src/main/java/com/thomaskioko/livedatademo/ui/viewmodel/MainActivityViewModel.java
@@ -1,24 +1,15 @@
 package com.thomaskioko.livedatademo.ui.viewmodel;
 
 import android.arch.lifecycle.LiveData;
-import android.arch.lifecycle.MutableLiveData;
+import android.arch.lifecycle.MediatorLiveData;
 import android.arch.lifecycle.ViewModel;
-import android.os.Handler;
 import android.support.annotation.NonNull;
 
-import com.thomaskioko.livedatademo.repository.RepositoryManager;
-import com.thomaskioko.livedatademo.repository.api.MovieResult;
-
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Random;
+import com.thomaskioko.livedatademo.repository.TmdbRepository;
+import com.thomaskioko.livedatademo.repository.api.ApiResponse;
 
 import javax.inject.Inject;
 
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
 import timber.log.Timber;
 
 /**
@@ -27,64 +18,31 @@ import timber.log.Timber;
 
 public class MainActivityViewModel extends ViewModel {
 
-    RepositoryManager mRepositoryManager;
-    private MutableLiveData<List<String>> mListMutableLiveData;
+    private MediatorLiveData<ApiResponse> mApiResponseMediatorLiveData;
+    private TmdbRepository tmdbRepository;
 
     @Inject
-    MainActivityViewModel(@NonNull RepositoryManager repositoryManager) {
-        mRepositoryManager = repositoryManager;
+    MainActivityViewModel(@NonNull TmdbRepository tmdbRepository) {
+        this.tmdbRepository = tmdbRepository;
     }
 
-
-    /**
-     * Helper method that returns a list of user names.
-     *
-     * @return {@link MutableLiveData} list of users
-     */
-    public LiveData<List<String>> getGitUserNames() {
-        if (mListMutableLiveData == null) {
-            mListMutableLiveData = new MutableLiveData<>();
-            loadUserNames();
+    public LiveData<ApiResponse> getPopularMovies() {
+        if (mApiResponseMediatorLiveData == null) {
+            mApiResponseMediatorLiveData = new MediatorLiveData<>();
+            loadPopularMovies();
         }
-        return mListMutableLiveData;
-    }
-
-    /**
-     * Helper method to simulate loading of data from a server. We will replace this later.
-     */
-    private void loadUserNames() {
-        Handler myHandler = new Handler();
-        myHandler.postDelayed(() -> {
-            List<String> fruitsStringList = new ArrayList<>();
-            fruitsStringList.add("@code_wizard");
-            fruitsStringList.add("@ninja_developer");
-            fruitsStringList.add("@denzel");
-            fruitsStringList.add("@bananaPeel");
-            fruitsStringList.add("@kioko");
-            long seed = System.nanoTime();
-            Collections.shuffle(fruitsStringList, new Random(seed));
-
-            mListMutableLiveData.setValue(fruitsStringList);
-        }, 5000);
-
+        return mApiResponseMediatorLiveData;
     }
 
     /**
      * Invoke {@link com.thomaskioko.livedatademo.repository.api.TmdbService} to fetch
      * list of popular movies
      */
-    public void getPopularMovies() {
-        mRepositoryManager.getPopularMovies().enqueue(new Callback<MovieResult>() {
-            @Override
-            public void onResponse(@NonNull Call<MovieResult> call, @NonNull Response<MovieResult> response) {
-                Timber.i("@getPopularMovies:: Status Code " + response.code());
-            }
-
-            @Override
-            public void onFailure(@NonNull Call<MovieResult> call, @NonNull Throwable t) {
-                Timber.e("@getPopularMovies:: " + t.getMessage());
-            }
-        });
+    private void loadPopularMovies() {
+        mApiResponseMediatorLiveData.addSource(
+                tmdbRepository.getPopularMovies(),
+                apiResponse -> mApiResponseMediatorLiveData.setValue(apiResponse)
+        );
     }
 
     @Override
