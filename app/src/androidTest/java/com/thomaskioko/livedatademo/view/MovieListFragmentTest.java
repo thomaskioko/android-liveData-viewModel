bb36080ecf37b5fef49d7179a83e0c1e329d5f24
Thomas Kioko <kiokotomas@gmail.com>
2018-01-12 16:17:14 +0300

    create  fragment test

---
+++ app/src/androidTest/java/com/thomaskioko/livedatademo/view/MovieListFragmentTest.java
@@ -0,0 +1,60 @@
+package com.thomaskioko.livedatademo.view;
+
+import android.arch.lifecycle.MediatorLiveData;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import com.thomaskioko.livedatademo.R;
+import com.thomaskioko.livedatademo.repository.model.ApiResponse;
+import com.thomaskioko.livedatademo.testing.SingleFragmentActivity;
+import com.thomaskioko.livedatademo.util.EspressoTestUtil;
+import com.thomaskioko.livedatademo.util.ViewModelUtil;
+import com.thomaskioko.livedatademo.view.ui.fragment.MovieListFragment;
+import com.thomaskioko.livedatademo.viewmodel.MovieListViewModel;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+/**
+ * @author Thomas Kioko
+ */
+
+@RunWith(AndroidJUnit4.class)
+public class MovieListFragmentTest {
+
+    @Rule
+    public ActivityTestRule<SingleFragmentActivity> activityTestRule =
+            new ActivityTestRule<>(SingleFragmentActivity.class, true, true);
+
+    private MovieListViewModel viewModel;
+    private MediatorLiveData<ApiResponse> mApiResponseMediatorLiveData = new MediatorLiveData<>();;
+
+    @Before
+    public void init(){
+        EspressoTestUtil.disableProgressBarAnimations(activityTestRule);
+        MovieListFragment movieListFragment = new MovieListFragment();
+
+        viewModel = mock(MovieListViewModel.class);
+        when(viewModel.getPopularMovies()).thenReturn(mApiResponseMediatorLiveData);
+
+        movieListFragment.viewModelFactory = ViewModelUtil.createFor(viewModel);
+
+        activityTestRule.getActivity().setFragment(movieListFragment);
+    }
+
+    @Test
+    public void loadResults(){
+        onView(withId(R.id.progressbar)).check(matches(isDisplayed()));
+    }
+
+}
