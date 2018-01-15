8ba982943110a22b7debf01a02d9b663c077627c
Thomas Kioko <kiokotomas@gmail.com>
2018-01-15 22:09:31 +0300

    inject view model

--- app/src/main/java/com/thomaskioko/livedatademo/viewmodel/MovieDetailViewModel.java
+++ app/src/main/java/com/thomaskioko/livedatademo/viewmodel/MovieDetailViewModel.java
@@ -2,7 +2,17 @@ package com.thomaskioko.livedatademo.viewmodel;
 
 import android.arch.lifecycle.ViewModel;
 
+import com.thomaskioko.livedatademo.repository.TmdbRepository;
+
+import javax.inject.Inject;
 
 
 public class MovieDetailViewModel extends ViewModel {
+
+    private TmdbRepository mTmdbRepository;
+
+    @Inject
+    MovieDetailViewModel(TmdbRepository tmdbRepository){
+        mTmdbRepository = tmdbRepository;
+    }
 }
