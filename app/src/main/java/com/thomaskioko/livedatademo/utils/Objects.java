991b1775b67a136ae163947e0807a51ae4125c06
Thomas Kioko <kiokotomas@gmail.com>
2018-01-22 14:46:25 +0300

    create util classes

---
+++ app/src/main/java/com/thomaskioko/livedatademo/utils/Objects.java
@@ -0,0 +1,13 @@
+package com.thomaskioko.livedatademo.utils;
+
+public class Objects {
+    public static boolean equals(Object o1, Object o2) {
+        if (o1 == null) {
+            return o2 == null;
+        }
+        if (o2 == null) {
+            return false;
+        }
+        return o1.equals(o2);
+    }
+}
