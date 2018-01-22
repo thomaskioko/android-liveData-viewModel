c06cdf67799068ca31fdc1bfbdf82d759b0e4ff6
Thomas Kioko <kiokotomas@gmail.com>
2018-01-22 17:13:48 +0300

    allow loading data from DB

--- app/src/main/java/com/thomaskioko/livedatademo/repository/util/NetworkBoundResource.java
+++ app/src/main/java/com/thomaskioko/livedatademo/repository/util/NetworkBoundResource.java
@@ -4,6 +4,7 @@ import android.arch.lifecycle.LiveData;
 import android.arch.lifecycle.MediatorLiveData;
 import android.support.annotation.MainThread;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.annotation.WorkerThread;
 
 import com.thomaskioko.livedatademo.repository.model.ApiResponse;
@@ -16,6 +17,7 @@ import com.thomaskioko.livedatademo.vo.Resource;
  * <p>
  * You can read more about it in the <a href="https://developer.android.com/arch">Architecture
  * Guide</a>.
+ *
  * @param <ResultType>
  * @param <RequestType>
  */
@@ -28,7 +30,15 @@ public abstract class NetworkBoundResource<ResultType, RequestType> {
     public NetworkBoundResource(AppExecutors appExecutors) {
         this.appExecutors = appExecutors;
         result.setValue(Resource.loading(null));
-        fetchFromNetwork();
+        LiveData<ResultType> dbSource = loadFromDb();
+        result.addSource(dbSource, data -> {
+            result.removeSource(dbSource);
+            if (shouldFetch(data)) {
+                fetchFromNetwork(dbSource);
+            } else {
+                result.addSource(dbSource, newData -> setValue(Resource.success(newData)));
+            }
+        });
     }
 
     @MainThread
@@ -38,12 +48,13 @@ public abstract class NetworkBoundResource<ResultType, RequestType> {
         }
     }
 
-    private void fetchFromNetwork() {
+    private void fetchFromNetwork(final LiveData<ResultType> dbSource) {
         LiveData<ApiResponse<RequestType>> apiResponse = createCall();
-        LiveData<ResultType> listLiveData = new MediatorLiveData<>();
-
+        // we re-attach dbSource as a new source, it will dispatch its latest value quickly
+        result.addSource(dbSource, newData -> setValue(Resource.loading(newData)));
         result.addSource(apiResponse, response -> {
             result.removeSource(apiResponse);
+            result.removeSource(dbSource);
             //noinspection ConstantConditions
             if (response.isSuccessful()) {
                 appExecutors.diskIO().execute(() -> {
@@ -52,13 +63,13 @@ public abstract class NetworkBoundResource<ResultType, RequestType> {
                             // we specially request a new live data,
                             // otherwise we will get immediately last cached value,
                             // which may not be updated with latest results received from network.
-                            result.addSource(listLiveData,
+                            result.addSource(loadFromDb(),
                                     newData -> setValue(Resource.success(newData)))
                     );
                 });
             } else {
                 onFetchFailed();
-                result.addSource(listLiveData,
+                result.addSource(dbSource,
                         newData -> setValue(Resource.error(response.errorMessage, newData)));
             }
         });
@@ -78,6 +89,13 @@ public abstract class NetworkBoundResource<ResultType, RequestType> {
 
     @WorkerThread
     protected abstract void saveCallResult(@NonNull RequestType item);
+
+    @MainThread
+    protected abstract boolean shouldFetch(@Nullable ResultType data);
+
+    @NonNull
+    @MainThread
+    protected abstract LiveData<ResultType> loadFromDb();
 
     @NonNull
     @MainThread
