ca4195bbf448e3b5fe3256b984cb5181063b0c06
Thomas Kioko <kiokotomas@gmail.com>
2018-01-23 13:38:18 +0300

    delete MovieSearchResult

--- app/src/main/java/com/thomaskioko/livedatademo/db/MovieDao.java
+++ app/src/main/java/com/thomaskioko/livedatademo/db/MovieDao.java
@@ -2,13 +2,11 @@ package com.thomaskioko.livedatademo.db;
 
 import android.arch.lifecycle.LiveData;
 import android.arch.persistence.room.Dao;
-import android.arch.persistence.room.Delete;
 import android.arch.persistence.room.Insert;
 import android.arch.persistence.room.OnConflictStrategy;
 import android.arch.persistence.room.Query;
 
 import com.thomaskioko.livedatademo.repository.model.Movie;
-import com.thomaskioko.livedatademo.vo.MovieSearchResult;
 
 import java.util.List;
 
