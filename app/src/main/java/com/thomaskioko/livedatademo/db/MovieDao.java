5e81caa03ba6fd7720fc039ea23ef337f045b540
Thomas Kioko <kiokotomas@gmail.com>
2018-01-23 13:37:11 +0300

    cleanup- remove unused class and function

--- app/src/main/java/com/thomaskioko/livedatademo/db/MovieDao.java
+++ app/src/main/java/com/thomaskioko/livedatademo/db/MovieDao.java
@@ -1,17 +1,15 @@
 package com.thomaskioko.livedatademo.db;
 
 import android.arch.lifecycle.LiveData;
-import android.arch.lifecycle.Transformations;
 import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Delete;
 import android.arch.persistence.room.Insert;
 import android.arch.persistence.room.OnConflictStrategy;
 import android.arch.persistence.room.Query;
-import android.util.SparseIntArray;
 
 import com.thomaskioko.livedatademo.repository.model.Movie;
 import com.thomaskioko.livedatademo.vo.MovieSearchResult;
 
-import java.util.Collections;
 import java.util.List;
 
 /**
@@ -26,26 +24,10 @@ public abstract class MovieDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     public abstract void insertMovies(List<Movie> movieList);
 
-    @Query("SELECT * FROM MovieSearchResult WHERE query = :query")
-    public abstract LiveData<MovieSearchResult> search(String query);
+    @Query("SELECT * FROM Movie WHERE query = :query")
+    public abstract LiveData<Movie> search(String query);
 
-    public LiveData<List<Movie>> loadOrdered(List<Integer> repoIds) {
-        SparseIntArray order = new SparseIntArray();
-        int index = 0;
-        for (Integer repoId : repoIds) {
-            order.put(repoId, index++);
-        }
-        return Transformations.map(loadById(repoIds), repositories -> {
-            Collections.sort(repositories, (r1, r2) -> {
-                int pos1 = order.get(r1.id);
-                int pos2 = order.get(r2.id);
-                return pos1 - pos2;
-            });
-            return repositories;
-        });
-    }
-
-    @Query("SELECT * FROM Movie WHERE id in (:repoIds)")
-    protected abstract LiveData<List<Movie>> loadById(List<Integer> repoIds);
+    @Query("DELETE FROM Movie")
+    public abstract void deleteAll();
 
 }
--- app/src/main/java/com/thomaskioko/livedatademo/db/TmdbDb.java
+++ app/src/main/java/com/thomaskioko/livedatademo/db/TmdbDb.java
@@ -4,12 +4,11 @@ import android.arch.persistence.room.Database;
 import android.arch.persistence.room.RoomDatabase;
 
 import com.thomaskioko.livedatademo.repository.model.Movie;
-import com.thomaskioko.livedatademo.vo.MovieSearchResult;
 
 /**
  * Main database description.
  */
-@Database(entities = {Movie.class, MovieSearchResult.class}, version = 1)
+@Database(entities = {Movie.class}, version = 1)
 public abstract class TmdbDb extends RoomDatabase {
 
     abstract public MovieDao movieDao();
