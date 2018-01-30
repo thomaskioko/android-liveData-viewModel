a5e02ee69cb663e9574e0b1092dc262b1db170c1
Thomas Kioko <kiokotomas@gmail.com>
2018-01-30 14:40:22 +0300

    implement view holder in search adapter

---
+++ app/src/main/java/com/thomaskioko/livedatademo/view/adapter/SearchItemAdapter.java
@@ -0,0 +1,70 @@
+package com.thomaskioko.livedatademo.view.adapter;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.thomaskioko.livedatademo.R;
+import com.thomaskioko.livedatademo.repository.model.Movie;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+
+public class SearchItemAdapter extends RecyclerView.Adapter<SearchItemAdapter.ViewHolder> {
+
+    private List<Movie> mMovieList = new ArrayList<>();
+
+
+    public SearchItemAdapter() {
+    }
+
+    @Override
+    public SearchItemAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.search_item, parent, false);
+        return new ViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(SearchItemAdapter.ViewHolder holder, int position) {
+        final Movie movie = mMovieList.get(position);
+
+        String posterUrl = holder.imageView.getContext().getString(R.string.tmdb_image_url) +
+                holder.imageView.getContext().getString(R.string.image_size_780) + movie.posterUrl;
+
+        holder.tvName.setText(movie.title);
+        Glide.with(holder.imageView.getContext())
+                .load(posterUrl)
+                .into(holder.imageView);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mMovieList.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        @BindView(R.id.movie_title)
+        TextView tvName;
+        @BindView(R.id.movie_thumb)
+        ImageView imageView;
+
+        public ViewHolder(View view) {
+            super(view);
+            ButterKnife.bind(this, view);
+        }
+    }
+
+
+    public void setItems(List<Movie> movieList) {
+        mMovieList = movieList;
+        notifyDataSetChanged();
+    }
+}
