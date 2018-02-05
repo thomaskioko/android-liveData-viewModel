c33196a59ee0d601173c6c60537b699629decbd2
Thomas Kioko <kiokotomas@gmail.com>
2018-02-05 12:50:09 +0300

    add method to load movie information.

--- app/src/main/java/com/thomaskioko/livedatademo/view/ui/fragment/MovieDetailFragment.java
+++ app/src/main/java/com/thomaskioko/livedatademo/view/ui/fragment/MovieDetailFragment.java
@@ -1,10 +1,14 @@
 package com.thomaskioko.livedatademo.view.ui.fragment;
 
 import android.arch.lifecycle.LifecycleFragment;
+import android.arch.lifecycle.ViewModelProvider;
+import android.arch.lifecycle.ViewModelProviders;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.CoordinatorLayout;
+import android.support.v7.graphics.Palette;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,9 +17,22 @@ import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.animation.GlideAnimation;
+import com.bumptech.glide.request.target.BitmapImageViewTarget;
 import com.mikhaellopez.circularprogressbar.CircularProgressBar;
 import com.thomaskioko.livedatademo.R;
 import com.thomaskioko.livedatademo.di.Injectable;
+import com.thomaskioko.livedatademo.repository.model.Movie;
+import com.thomaskioko.livedatademo.viewmodel.MovieDetailViewModel;
+
+import org.joda.time.LocalDate;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+
+import java.util.Locale;
+
+import javax.inject.Inject;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -25,6 +42,9 @@ import butterknife.ButterKnife;
  */
 
 public class MovieDetailFragment extends LifecycleFragment implements Injectable {
+
+    @Inject
+    ViewModelProvider.Factory viewModelFactory;
 
     @BindView(R.id.layout_movie_title)
     RelativeLayout mRelativeLayout;
@@ -70,5 +90,83 @@ public class MovieDetailFragment extends LifecycleFragment implements Injectable
         args.putInt(BUNDLE_MOVIE_ID, movieId);
         fragment.setArguments(args);
         return fragment;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        MovieDetailViewModel movieDetailViewModel = ViewModelProviders.of(this, viewModelFactory)
+                .get(MovieDetailViewModel.class);
+
+        movieDetailViewModel.setMovieId(getArguments().getInt(BUNDLE_MOVIE_ID));
+        movieDetailViewModel.getMovie().observe(this, movieResource -> {
+            if (movieResource != null) {
+                handleResponse(movieResource.data);
+            }
+        });
+    }
+
+    private void handleResponse(Movie mMovieResult) {
+        //Image path
+        String imagePath = getString(R.string.tmdb_image_url) +
+                getString(R.string.image_size_780) + mMovieResult.posterUrl;
+
+        //Backdrop Image URL
+        String imagePathBackDrop = getString(R.string.tmdb_image_url) +
+                getString(R.string.image_size_780)
+                + mMovieResult.backdropPath;
+
+        Glide.with(getActivity())
+                .load(imagePath)
+                .asBitmap()
+                .centerCrop()
+                .into(mThumbnail);
+
+        float rating = mMovieResult.voteAverage.floatValue() * 10;
+        float popularity = mMovieResult.popularity.intValue();
+
+        DateTimeFormatter formatter = DateTimeFormat.forPattern("yyyy-MM-dd")
+                .withLocale(Locale.getDefault());
+
+        //Get the year from the release date.
+        LocalDate date = formatter.parseLocalDate(mMovieResult.releaseYear);
+
+        mMoviePlot.setText(mMovieResult.overview);
+        mMovieYear.setText(String.valueOf(date.getYear()));
+        mMovieRating.setText(String.valueOf(mMovieResult.voteAverage));
+        mMoviePopularity.setText(String.valueOf(popularity));
+        mMovieVote.setText(String.valueOf(mMovieResult.voteCount));
+        mCircularProgressBar.setProgressWithAnimation(rating);
+
+        appBarLayout.setTitle(mMovieResult.title);
+
+
+        Glide.with(imageView.getContext())
+                .load(imagePathBackDrop)
+                .asBitmap()
+                .into(new BitmapImageViewTarget(imageView) {
+                    @Override
+                    public void onResourceReady(Bitmap bitmap, final GlideAnimation glideAnimation) {
+                        super.onResourceReady(bitmap, glideAnimation);
+                        Palette.from(bitmap).generate(palette -> {
+
+                            if (palette.getDarkVibrantSwatch() != null) {
+                                mRelativeLayout.setBackgroundColor(palette.getDarkVibrantSwatch().getRgb());
+                                mCircularProgressBar.setBackgroundColor(palette.getDarkVibrantSwatch().getRgb());
+
+                            } else if (palette.getMutedSwatch() != null) {
+                                mRelativeLayout.setBackgroundColor(palette.getMutedSwatch().getRgb());
+                                mCircularProgressBar.setBackgroundColor(palette.getMutedSwatch().getRgb());
+                            }
+                            if (palette.getLightVibrantSwatch() != null) {
+                                mCircularProgressBar.setColor(palette.getLightVibrantSwatch().getRgb());
+                            } else if (palette.getLightMutedSwatch() != null) {
+                                mCircularProgressBar.setColor(palette.getLightMutedSwatch().getRgb());
+                            }
+                        });
+                    }
+                });
+
     }
 }
