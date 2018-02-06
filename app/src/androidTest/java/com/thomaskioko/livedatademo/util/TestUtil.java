b252467f23b9514926c845f94f86ec4ea00bcfec
Thomas Kioko <kiokotomas@gmail.com>
2018-02-06 10:36:47 +0300

    update parameters

--- app/src/androidTest/java/com/thomaskioko/livedatademo/util/TestUtil.java
+++ app/src/androidTest/java/com/thomaskioko/livedatademo/util/TestUtil.java
@@ -13,7 +13,9 @@ import java.util.List;
 public class TestUtil {
 
     public static Movie createMovie(String title, String posterPath) {
-        return new Movie(346364, posterPath, 7.8, "2017", title);
+        return new Movie(346364, posterPath, 7.8, "2017", title,
+                null, null, null, null, null,
+                null, null, null, null);
     }
 
     public static MovieResult createMovieResult(Integer page, List<Movie> movieList) {
@@ -24,11 +26,11 @@ public class TestUtil {
         return movieResult;
     }
 
-    public static List<Movie> getMovieList(){
+    public static List<Movie> getMovieList() {
         List<Movie> movieList = new ArrayList<>();
         movieList.add(createMovie("Star Wars: The Last Jedi", "\\/9E2y5Q7WlCVNEhP5GiVTjhEhx1o.jpg"));
         movieList.add(createMovie("Star Wars: The Last Jedi", "\\/47pLZ1gr63WaciDfHCpmoiXJlVr.jpg"));
 
-        return  movieList;
+        return movieList;
     }
 }
